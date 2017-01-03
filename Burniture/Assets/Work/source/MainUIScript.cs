26ab020294dde24972f9624d54de56c608fce3ca
JangWon <sda362002@naver.com>
2017-01-04 00:10:30 +0900

    please accept

---
+++ Burniture/Assets/Work/source/MainUIScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUIScript : MonoBehaviour {
+
+	public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
