1810eee612e2e137b3d980dbaf02085a9e51ae70
stop0128 <stoooooping@gmail.com>
2017-01-15 15:42:35 +0900

    stop_Newdrag2

--- Burniture/Assets/Work/source/NewDrag.cs
+++ Burniture/Assets/Work/source/NewDrag.cs
@@ -17,27 +17,23 @@ public class NewDrag : MonoBehaviour
     public GameObject Sphere6;
     public GameObject Sphere7;
     public GameObject Sphere8;
-    public GameObject MoveSphere;
-    public int sphere_num = 0;
-    public int state = stop;
-    static int move = 0;
-    static int stop = 1;
+    public GameObject MoveSphere;               // 움직일 점을 저장할 클래스
+    public int sphere_num = 0;                  // 선택된 점의 번호
 
     void Update()
     {
-        if (Input.touchCount == 0)
+        if (Input.touchCount == 0)              // 터치가 없으면
         {
-            state = stop;
+            sphere_num = 0;                     // 선택된 것도 없음
+            MoveSphere = null;
         }
-        //if (Input.touchCount == 1 && state == stop) // 화면에 터치한 손가락의 갯수가 한개일때
-        if (Input.touchCount == 1) // 화면에 터치한 손가락의 갯수가 한개일때
+        if (Input.touchCount == 1)              // 화면에 터치한 손가락의 갯수가 한개일때
         {
-            //Debug.Log(Input.mousePosition);
 
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); // 손가락에서 화면안으로 레이저를쏨
             RaycastHit hit = new RaycastHit(); // 레이저가 맞을때를 hit라고 선언
 
-            if (Physics.Raycast(ray, out hit)) // 레이저가 오브젝트에 맞았는가?
+            if (Physics.Raycast(ray, out hit) && sphere_num == 0) // 레이저가 오브젝트에 맞고, 아직 선택된 것이 없을때
             {
                 Debug.Log("collide");
                 Debug.Log(hit.collider.gameObject.tag);
@@ -81,62 +77,12 @@ public class NewDrag : MonoBehaviour
                     sphere_num = 8;
                     MoveSphere = Sphere8;
                 }
-                /*else
-                {
-                    sphere_num = 0;
-                    MoveSphere = null;
-                }*/
             }
             else
             {
-                state = move;
                 var touchDeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                 MoveSphere.transform.Translate(touchDeltaPosition.x * Speed * Time.deltaTime, 0, touchDeltaPosition.y * Speed * Time.deltaTime);
             }
-            /*
-            if (hit.collider.gameObject.tag == "Sphere1" && Input.GetTouch(0).phase == TouchPhase.Moved) // 레이저와 충돌안 오브젝트의 태그가 Sphere1이고 첫번째 터치상태가 움직일때
-            {
-                /* 오브젝트를 Speed의 속도로 움직임*//*
-                var touchDeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere1.transform.Translate(touchDeltaPosition.x * Speed * Time.deltaTime, 0, touchDeltaPosition.y * Speed * Time.deltaTime);
-
-            }
-            else if (hit.collider.gameObject.tag == "Sphere2" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere2.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-
-            else if (hit.collider.gameObject.tag == "Sphere3" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere3.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-            else if (hit.collider.gameObject.tag == "Sphere4" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere4.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-            else if (hit.collider.gameObject.tag == "Sphere5" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere5.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-            else if (hit.collider.gameObject.tag == "Sphere6" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere6.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-            else if (hit.collider.gameObject.tag == "Sphere7" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere7.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }
-            else if (hit.collider.gameObject.tag == "Sphere8" && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
-                Sphere8.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
-            }*/
         }
     }
 }
