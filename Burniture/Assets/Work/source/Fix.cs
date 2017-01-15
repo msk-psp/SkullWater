64cc062aeb016aef6278c7faa7a1a4589a53b2db
JangWon <sda362002@naver.com>
2017-01-15 15:18:09 +0900

    asd

--- Burniture/Assets/Work/source/Fix.cs
+++ Burniture/Assets/Work/source/Fix.cs
@@ -10,48 +10,37 @@ public class Fix : MonoBehaviour {
     public GameObject Sphere6;
     public GameObject Sphere7;
     public GameObject Sphere8;
-    Vector3 v1,v2,v3,v4;
-    /*void start()
+    Vector3 v;
+
+    public void FixCube()
     {
-        Sphere1 = GameObject.Find("Sphere1");
-        Sphere2 = GameObject.Find("Sphere2");
-        Sphere3 = GameObject.Find("Sphere3");
-        Sphere4 = GameObject.Find("Sphere4");
-        Sphere5 = GameObject.Find("Sphere5");
-        Sphere6 = GameObject.Find("Sphere6");
-        Sphere7 = GameObject.Find("Sphere7");
-        Sphere8 = GameObject.Find("Sphere8");
-    }*/
-    public void asd()
-    {
-        Debug.Log("ad");
-    }
-    public void onClick()
-    {
-        Debug.Log("Akkkkkkkkkkkkkkkkk");
         //Shere1,2,3,7은 안해도됨
-        v1 = transform.position;
-        v1.x = Sphere1.transform.position.x;
-        v1.y = Sphere3.transform.position.y;
-        v1.z = Sphere1.transform.position.z;
-        transform.position = v1;
-        Sphere4.transform.position = v1;
-        v2 = transform.position;
-        v2.x = Sphere1.transform.position.x;
-        v2.y = Sphere3.transform.position.y;
-        v2.z = Sphere7.transform.position.z;
-        transform.position = v2;
-        Sphere5.transform.position = v2;
-        v3 = transform.position;
-        v3.x = Sphere3.transform.position.x;
-        v3.y = Sphere3.transform.position.y;
-        v3.z = Sphere7.transform.position.z;
-        Sphere6.transform.position = v3;
-        v4 = transform.position;
-        v4.x = Sphere1.transform.position.x;
-        v4.y = Sphere1.transform.position.y;
-        v4.z = Sphere7.transform.position.z;
-        Sphere8.transform.position = v4;
-        Debug.Log("Akkkkkkkkkkkkkkkkk");
+        v = transform.position;
+
+        v.x = Sphere1.transform.position.x;
+        v.y = Sphere3.transform.position.y;
+        v.z = Sphere3.transform.position.z;
+        transform.position = v;
+        Sphere4.transform.position = v;
+
+        //v2 = transform.position;
+        v.x = Sphere1.transform.position.x;
+        v.y = Sphere3.transform.position.y;
+        v.z = Sphere7.transform.position.z;
+        transform.position = v;
+        Sphere5.transform.position = v;
+
+        //v3 = transform.position;
+        v.x = Sphere3.transform.position.x;
+        v.y = Sphere3.transform.position.y;
+        v.z = Sphere7.transform.position.z;
+        transform.position = v;
+        Sphere6.transform.position = v;
+
+        //v4 = transform.position;
+        v.x = Sphere1.transform.position.x;
+        v.y = Sphere1.transform.position.y;
+        v.z = Sphere7.transform.position.z;
+        Sphere8.transform.position = v;
     }
 }
--- Burniture/Assets/Work/source/NewDrag.cs
+++ Burniture/Assets/Work/source/NewDrag.cs
@@ -32,45 +32,45 @@ public class NewDrag : MonoBehaviour
             {
                 Debug.Log("collide");
                 Debug.Log(hit.collider.gameObject.tag);
-                if (hit.collider.gameObject.tag == "Sphere1" && Input.GetTouch(0).phase == TouchPhase.Moved) // 레이저와 충돌안 오브젝트의 태그가 Sphere1이고 첫번째 터치상태가 움직일때
+                if (hit.collider.gameObject.tag == "Sphere1") // 레이저와 충돌안 오브젝트의 태그가 Sphere1이고 첫번째 터치상태가 움직일때
                 {
                     /* 오브젝트를 Speed의 속도로 움직임*/
                     var touchDeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere1.transform.Translate(touchDeltaPosition.x * Speed * Time.deltaTime, 0, touchDeltaPosition.y * Speed * Time.deltaTime);
 
                 }
-                else if (hit.collider.gameObject.tag == "Sphere2" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere2")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere2.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
 
-                else if (hit.collider.gameObject.tag == "Sphere3" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere3")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere3.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
-                else if (hit.collider.gameObject.tag == "Sphere4" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere4")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere4.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
-                else if (hit.collider.gameObject.tag == "Sphere5" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere5")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere5.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
-                else if (hit.collider.gameObject.tag == "Sphere6" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere6")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere6.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
-                else if (hit.collider.gameObject.tag == "Sphere7" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere7")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere7.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
                 }
-                else if (hit.collider.gameObject.tag == "Sphere8" && Input.GetTouch(0).phase == TouchPhase.Moved)
+                else if (hit.collider.gameObject.tag == "Sphere8")
                 {
                     var DeltaPosition = (Vector3)Input.GetTouch(0).deltaPosition;
                     Sphere8.transform.Translate(DeltaPosition.x * Speed * Time.deltaTime, 0, DeltaPosition.y * Speed * Time.deltaTime);
