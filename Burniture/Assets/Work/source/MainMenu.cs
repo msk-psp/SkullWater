bce6d0cc73eeb20d2b780de7ea59b315f77e85ab
JangWon <sda362002@naver.com>
2017-01-25 15:35:52 +0900

    Recently

--- Burniture/Assets/Work/source/MainMenu.cs
+++ Burniture/Assets/Work/source/MainMenu.cs
@@ -1,10 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+
+    public AndroidJavaObject and;
 
     public void ChangeScene(string sceneName)
     {
         Application.LoadLevel(sceneName);
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        AndroidJNI.AttachCurrentThread();
+        AndroidJavaClass AJC = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        and = AJC.GetStatic<AndroidJavaObject>("currentActivity");
+    }
+    void Update()
+    {
+        and.Call("ToastMessage");
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+             Application.Quit();
+        }
     }
 }
