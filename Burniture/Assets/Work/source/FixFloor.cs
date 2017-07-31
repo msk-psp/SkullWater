bf5f645d256e065cd12e3796333a2c48315f26c4
ST04 <sda362002@naver.com>
2017-07-31 11:15:43 +0900

    0731_Ojang

--- Burniture/Assets/Work/source/FixFloor.cs
+++ Burniture/Assets/Work/source/FixFloor.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using Vuforia;
-public class FixFloor : MonoBehaviour  {
-    
+
+public class FixFloor : MonoBehaviour
+{
+
     private GameObject[] Spheres;
     private GameObject[] Lines;
     private const string SPHERE_TAG_NAME = "Sphere";
@@ -9,51 +11,69 @@ public class FixFloor : MonoBehaviour  {
     private const int SPHERE_NUMBER = 8;
     private const string IMAGETARGET_NAME = "ImageTarget";
     private const string ARCAMERA_NAME = "ARCamera";
-	public void fixfloor()
+    private GameObject NParent; // ㅇㅇ
+    //VuforiaBehaviour qcarBehaviour;
+    public GameObject obj;
+
+
+    public void fixfloor()
     {
-        GameObject obj;
         
+        NParent = GameObject.Find("Copy"); // ㅇㅇ
+
         Spheres = new GameObject[SPHERE_NUMBER];
         Lines = new GameObject[GameObject.FindGameObjectsWithTag(LINE_TAG_NAME).Length];
         Lines = GameObject.FindGameObjectsWithTag(LINE_TAG_NAME);
-        
+
+        NParent.transform.position = GameObject.Find(IMAGETARGET_NAME).transform.position ; // ㅇㅇ
 
         for (int i = 0; i < SPHERE_NUMBER; i++)
         {
-            Spheres[i] = GameObject.FindGameObjectWithTag(SPHERE_TAG_NAME + (i+1));
-            obj = Instantiate(Spheres[i]);
-            obj.transform.position = Spheres[i].transform.position;
-            Spheres[i].SetActive(false);
+            Spheres[i] = GameObject.FindGameObjectWithTag(SPHERE_TAG_NAME + (i + 1));
+            //obj = Instantiate(Spheres[i]);
+            obj = Spheres[i];
+            //obj.transform.position = Spheres[i].transform.position;
+            obj.transform.parent = NParent.transform; // 부모바꾸기
+            //Spheres[i].SetActive(false);
         }
         string[] name = new string[Lines.Length]; ;
         for (int i = 0; i < Lines.Length; i++)
         {
-            obj = Instantiate(Lines[i]);
-            obj.transform.position = Lines[i].transform.position;
+            //obj = Instantiate(Lines[i]);
+            //obj.transform.position = Lines[i].transform.position;
+            obj = Lines[i];
             name[i] = Lines[i].GetComponent<Line>().GetNames();
-            Lines[i].SetActive(false);
+            obj.transform.parent = NParent.transform; // 부모바꾸기
+            //Lines[i].SetActive(false);
         }
-        GameObject ground= GameObject.Find("Ground");
-        obj = Instantiate(ground);
-        obj.transform.position = ground.transform.position;
-        ground.SetActive(false);
+        GameObject ground = GameObject.Find("Ground");
+        //obj = Instantiate(ground);
+        //obj.transform.position = ground.transform.position;
+        obj = ground;
+        obj.transform.parent = NParent.transform; // 부모바꾸기
+        //ground.SetActive(false);
 
-        GameObject.Find(IMAGETARGET_NAME).GetComponent<DefaultTrackableEventHandler>().ChangeObjects();
-        GameObject.Find(ARCAMERA_NAME).GetComponent<Drag>().ChangeObjects();
+        //GameObject.Find(IMAGETARGET_NAME).GetComponent<DefaultTrackableEventHandler>().ChangeObjects();
+        //GameObject.Find(ARCAMERA_NAME).GetComponent<Drag>().ChangeObjects();
         Lines = GameObject.FindGameObjectsWithTag(LINE_TAG_NAME);   //find line without deactivated gameobject
-        
+
         for (int i = 0; i < Lines.Length; i++)
         {
-            string[]  splitNames = name[i].Split(',');
-            Lines[i].GetComponent<Line>().ChangeObjects(splitNames[0],splitNames[1]);
+            string[] splitNames = name[i].Split(',');
+            Lines[i].GetComponent<Line>().ChangeObjects(splitNames[0], splitNames[1]);
             if (Lines[i].GetComponent<Distance>())
             {
                 Lines[i].GetComponent<Distance>().ChangeObjects(splitNames[0], splitNames[1]);
             }
         }
 
-            TrackerManager.Instance.GetTracker<ObjectTracker>().PersistExtendedTracking(true);
-        //TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
-        
+        //TrackerManager.Instance.GetTracker<ObjectTracker>().PersistExtendedTracking(true);
+        //VuforiaBehaviour.Instance.enabled = false;
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+        /* qcarBehaviour = (VuforiaBehaviour)UnityEngine.Object.FindObjectOfType(
+                 typeof(VuforiaBehaviour));
+         qcarBehaviour.enabled = false;*/
+        GameObject.Find(ARCAMERA_NAME).GetComponent<Gyro>().enabled = true;
+        //GameObject.Find(ARCAMERA_NAME).GetComponent<KeepScale>().enabled = true;
     }
 }
--- Burniture/Assets/Work/source/Gyro.cs
+++ Burniture/Assets/Work/source/Gyro.cs
@@ -3,19 +3,17 @@ using System.Collections;
 using Vuforia;
 
 public class Gyro : MonoBehaviour {
-
     // Use this for initialization
     void Start()
     {
         VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
         VuforiaBehaviour.Instance.RegisterOnPauseCallback(OnPaused); // 카메라 초점모드
+
         Input.gyro.enabled = true;
-        Input.gyro.updateInterval = 0.01f;
     }
     private void OnVuforiaStarted() // 카메라
     {
-        bool focusModeSet = CameraDevice.Instance.SetFocusMode(
-     CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
 
         if (!focusModeSet)
         {
@@ -35,8 +33,6 @@ public class Gyro : MonoBehaviour {
 
     void Update()
     {
-        transform.Rotate(-Input.gyro.rotationRateUnbiased.x,
-                          -Input.gyro.rotationRateUnbiased.y,
-                          0);
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
     }
 }
